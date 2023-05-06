63faeb6c015145e0e46b19324e8b5ab747e08671
unknown <johannjerman014@gmail.com>
2023-05-06 17:28:59 -0300

    usestate y useeffect

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react"
+
+const API = 'http://localhost:3004/api/user'
+
 export default function Users() {
+    let [ users, setUsers ] = useState([])
+
+    useEffect( () => {
+
+      userRequest(API)
+
+    }, [] )
+    
+    const userRequest = async (api) => {
+      let res = await fetch(api)
+      let data = await res.json()
+      setUsers(data.data)
+    }
+
     return (
       <>
-      <p>Estos son todos los usuarios</p>
+        <h3>Estos son todos los usuarios</h3>
+        { users.length === 0 && <p>Cargando...</p> }
+        { 
+          users?.map( (user, i) => {
+            return (
+              <p key={user.name + i}> {user.name} </p>
+            )
+          })
+        }
       </>
     )
   }
